2ea33211441a3dab897f6114527de1e8c1a975e2
BaironDev1 <baironampuero2468@gmail.com>
2025-05-30 17:22:16 -0400

    Configuracion Deploy1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
-import staticAdapter from '@astrojs/static';
+
 
 export default defineConfig({
   vite: {
@@ -15,7 +15,6 @@ export default defineConfig({
     tailwind(),
     icon(),
     react() // <-- ¡AÑADE LA INTEGRACIÓN DE REACT AQUÍ!
-  ],
-  output: 'static',
-  adapter: staticAdapter(),
+  ]
+  
 });
